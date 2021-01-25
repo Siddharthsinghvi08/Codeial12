6e6076fa1ec5d3c8f09ecc331c312c2830cf10fa
Siddharthsinghvi08 <66488055+Siddharthsinghvi08@users.noreply.github.com>
2021-01-25 22:15:05 +0530

    Update chat_engine.js

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -2,7 +2,7 @@ class ChatEngine{
     constructor(chatBoxId,userEmail){
         this.chatBox=$(`#${chatBoxId}`);
         this.userEmail=userEmail;
-        this.socket=io.connect('http://localhost:5000');
+        this.socket=io.connect('http://65.0.133.135:5000');
 
         if(this.userEmail){
             this.connectionHandler();
